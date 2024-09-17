aaef96f60fbf3d4dad36db2bab929b3b1a699683
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-09-17 16:21:41 +0200

    Add files via upload

--- sprint2/problems/command_line/solution/src/json_loader.cpp
+++ sprint2/problems/command_line/solution/src/json_loader.cpp
@@ -31,7 +31,9 @@ model::Game LoadGame(const fs::path& config_file) {
     double default_dog_speed = DEFAULT_DOG_SPEED;
     try {
         default_dog_speed = config.at("defaultDogSpeed").as_double();
-    } catch (...) { }
+    } catch (...) {
+        throw std::runtime_error("Wrong config json: defaultDogSpeed");
+        }
 
     try {
         for (auto& json_map : config.at("maps").as_array()) {
@@ -41,45 +43,21 @@ model::Game LoadGame(const fs::path& config_file) {
             double dog_speed = default_dog_speed;
             try {
                 dog_speed = json_map.at("dogSpeed").as_double();
-            } catch (...) { }
+            } catch (...) {
+                throw std::runtime_error("Wrong config json: dogSpeed");
+                }
+
             model::Map map{model::Map::Id(id), name, dog_speed};
 
             json::array roads = json_map.as_object().at("roads").as_array();
-            for (const auto& road : roads) {
-                int x0 = json::value_to< int >(road.as_object().at("x0"));
-                int y0 = json::value_to< int >(road.as_object().at("y0"));
-                try {
-                    int x1 = json::value_to< int >(road.as_object().at("x1"));
-                    model::Road road(model::Road::HORIZONTAL, {x0, y0}, x1);
-                    map.AddRoad(road);
-                } catch (...) { }
-                try {
-                    int y1 = json::value_to< int >(road.as_object().at("y1"));
-                    model::Road road(model::Road::VERTICAL, {x0, y0}, y1);
-                    map.AddRoad(road);
-                } catch (...) { }
-            }
+            LoadRoads(roads,map);
 
             json::array buildings = json_map.as_object().at("buildings").as_array();
-            for (const auto& building : buildings) {
-                int x = json::value_to< int >(building.as_object().at("x"));
-                int y = json::value_to< int >(building.as_object().at("y"));
-                int w = json::value_to< int >(building.as_object().at("w"));
-                int h = json::value_to< int >(building.as_object().at("h"));
-                model::Building build(model::Rectangle({x, y}, {w, h}));
-                map.AddBuilding(build);
-            }
+            LoadBuildings(buildings, map);
 
             json::array offices = json_map.as_object().at("offices").as_array();
-            for (const auto& office : offices) {
-                std::string id = json::value_to< std::string >(office.as_object().at("id"));
-                int x = json::value_to< int >(office.as_object().at("x"));
-                int y = json::value_to< int >(office.as_object().at("y"));
-                int offset_x = json::value_to< int >(office.as_object().at("offsetX"));
-                int offset_y = json::value_to< int >(office.as_object().at("offsetY"));
-                model::Office offc(model::Office::Id(id), {x, y}, {offset_x, offset_y});
-                map.AddOffice(offc);
-            }
+            LoadOffices(offices, map);
+
             game.AddMap(map);
         }
     } catch (...) {
@@ -87,6 +65,51 @@ model::Game LoadGame(const fs::path& config_file) {
     }
 
     return game;
+}
+
+void LoadRoads(const boost::json::array& roads, model::Map& map){
+    for (const auto& road : roads) {
+        int x0 = json::value_to< int >(road.as_object().at("x0"));
+        int y0 = json::value_to< int >(road.as_object().at("y0"));
+        try {
+            int x1 = json::value_to< int >(road.as_object().at("x1"));
+            model::Road road(model::Road::HORIZONTAL, {x0, y0}, x1);
+            map.AddRoad(road);
+        } catch (...) {
+            throw std::runtime_error("Wrong config json: roads");
+            }
+
+        try {
+            int y1 = json::value_to< int >(road.as_object().at("y1"));
+            model::Road road(model::Road::VERTICAL, {x0, y0}, y1);
+            map.AddRoad(road);
+        } catch (...) {
+            throw std::runtime_error("Wrong config json: roads");
+            }
+    }
+}
+
+void LoadBuildings(const boost::json::array& buildings, model::Map& map){
+    for (const auto& building : buildings) {
+        int x = json::value_to< int >(building.as_object().at("x"));
+        int y = json::value_to< int >(building.as_object().at("y"));
+        int w = json::value_to< int >(building.as_object().at("w"));
+        int h = json::value_to< int >(building.as_object().at("h"));
+        model::Building build(model::Rectangle({x, y}, {w, h}));
+        map.AddBuilding(build);
+    }
+}
+
+void LoadOffices(const boost::json::array& offices, model::Map& map){
+    for (const auto& office : offices) {
+        std::string id = json::value_to< std::string >(office.as_object().at("id"));
+        int x = json::value_to< int >(office.as_object().at("x"));
+        int y = json::value_to< int >(office.as_object().at("y"));
+        int offset_x = json::value_to< int >(office.as_object().at("offsetX"));
+        int offset_y = json::value_to< int >(office.as_object().at("offsetY"));
+        model::Office offc(model::Office::Id(id), {x, y}, {offset_x, offset_y});
+        map.AddOffice(offc);
+    }
 }
 
 }  // namespace json_loader
--- sprint2/problems/command_line/solution/src/json_loader.h
+++ sprint2/problems/command_line/solution/src/json_loader.h
@@ -14,4 +14,8 @@ namespace json_loader {
 
 model::Game LoadGame(const std::filesystem::path& config_file);
 
+void LoadRoads(const boost::json::array& roads, model::Map& map);
+void LoadBuildings(const boost::json::array& buildings, model::Map& map);
+void LoadOffices(const boost::json::array& offices, model::Map& map);
+
 }  // namespace json_loader
