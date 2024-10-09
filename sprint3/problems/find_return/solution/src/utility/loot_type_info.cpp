363f79cfe8ce0ff3641f51046554ac757571d290
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-10-10 00:55:46 +0300

    Delete sprint3/problems directory

--- sprint3/problems/find_return/solution/src/utility/loot_type_info.cpp
+++
@@ -1,9 +0,0 @@
-#include "loot_type_info.h"
-
-namespace loot_type_info {
-	const json::array& LootTypeInfo::GetInfo(const std::string& map_name) const {
-		static const json::array empty_array;
-		auto it = map_to_info_.find(map_name);
-		return it != map_to_info_.end() ? it->second : empty_array;
-	}
-}
