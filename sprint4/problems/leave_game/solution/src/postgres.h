e274b1ec71d653afbbd282ff456d3d6bd4e82b8c
Nikita <137328449+Z1rea3L@users.noreply.github.com>
2024-10-24 22:23:25 +0200

    aaaaaaaaaaaaaaaaaaaaaaa

---
+++ sprint4/problems/leave_game/solution/src/postgres.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <pqxx/connection>
+#include <pqxx/transaction>
+#include "model.h"
+
+namespace postgres {
+class RetiredRepositoryImpl{
+    
+public:
+    explicit RetiredRepositoryImpl(pqxx::connection& connection)
+        : connection_{connection}
+    {}
+
+    void SaveRetired(const model::PlayerRecordItem& retired);
+    std::vector<model::PlayerRecordItem> GetRetired(int start = 0, int max_items = 100);
+private:
+    pqxx::connection& connection_;
+};
+
+class Database {
+public:
+    explicit Database(pqxx::connection connection);
+    void CreateTable();
+private:
+    pqxx::connection connection_;
+};
+
+}  // namespace postgres
